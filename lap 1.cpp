2b0739a6bfbcaff18a0ff3a67022e602b4587991
6704062610267 <s6704062610267@email.kmutnb.ac.th>
2024-11-28 14:54:53 +0700

    commit 4 done
    deee

--- lap 1.cpp
+++ lap 1.cpp
@@ -3,7 +3,7 @@ int main(){
 	int num1;
 	int num2;
 	printf("num1=<Enter num1> \nnum2=<Enter num2>\n");
-	scanf("%d %d", &num1,&num2);
+	scanf("%d \n %d", &num1,&num2);
 	
 	if (num1>num2) {
 		printf("num1 greater than num2");
