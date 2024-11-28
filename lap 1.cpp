d52a9b1cfbbe1c6615cceb25250312518ce35e0a
6704062610267 <s6704062610267@email.kmutnb.ac.th>
2024-11-28 14:53:17 +0700

    commit last update3
    แก้แล้ว

--- lap 1.cpp
+++ lap 1.cpp
@@ -3,8 +3,8 @@ int main(){
 	int num1;
 	int num2;
 	printf("num1=<Enter num1> \nnum2=<Enter num2>\n");
-	scanf("%d", &num1);
-	scanf("%d", &num2);
+	scanf("%d %d", &num1,&num2);
+	
 	if (num1>num2) {
 		printf("num1 greater than num2");
 	}
